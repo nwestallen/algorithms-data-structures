b7887a1358124c86529e4d6c74d2fcb730d62012
Nick Allen <nwestallen@gmail.com>
2020-06-06 14:09:32 -0500

    Completed merge sort

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 - ~~learn how to represent floating point numbers in binary~~
 
 ## data structures
-- learn how to use a stack and queue in python
+- ~~learn how to use a stack and queue in python~~
 - ~~learn how to make and traverse a linked list~~
 
 ## algorithms
-- learn how to make a merge sort on your own
+- ~~learn how to make a merge sort on your own~~
 
 ## vim
 - ~~open powershell and type `vimtutor` to learn vim~~
--- merge_sort.py
+++ merge_sort.py
@@ -1,7 +1,13 @@
 # Merge sort algorithm
 
-list_1 = [4, 5, 6, 7, 8]
-list_2 = [0, 1, 2, 3]
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    left = lst[0:len(lst)//2]
+    right = lst[len(lst)//2:]
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return merge(left, right)
 
 
 def merge(left, right):
@@ -20,5 +26,7 @@ def merge(left, right):
 
 list_1 = [4, 5, 6, 7, 8]
 list_2 = [0, 1, 2, 3]
+list_3 = [11, 2, 4, 3, 1, 5, 24, 13, 2, -3, 0, 120]
 
 print(merge(list_1, list_2))
+print(merge_sort(list_3))
