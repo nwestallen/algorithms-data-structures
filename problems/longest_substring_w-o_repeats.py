3f2876275f82b0f08609d6ea9add9b92e363242d
Nick <nwestallen@gmail.com>
2021-06-27 20:47:52 -0400

    longest substring

---
+++ problems/longest_substring_w-o_repeats.py
@@ -0,0 +1,32 @@
+#https://leetcode.com/submissions/detail/508592080/
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) == 0:
+            return 0
+        
+        i = 0
+        word = ''
+        maxlen = 0
+        
+        while i < len(s):
+            j = i + 1
+            word = s[i]
+            while j < len(s):
+                #print(i, j, word)
+                if s[j] in word:
+                    #print(s[j])
+                    if len(word) > maxlen:
+                        maxlen = len(word)
+                    break
+                else:
+                    word += s[j]
+                j += 1
+            if len(word) > maxlen:
+                maxlen = len(word)
+            i += 1
+        return maxlen
