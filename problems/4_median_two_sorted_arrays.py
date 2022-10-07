47ac322bee2fac377dcf4d940f506838cf12c4a8
Nick <nwestallen@gmail.com>
2022-10-07 16:19:04 -0400

    old changes to problem 4

--- problems/4_median_two_sorted_arrays.py
+++ problems/4_median_two_sorted_arrays.py
@@ -3,7 +3,7 @@
 from typing import List
 
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays_brute(self, nums1: List[int], nums2: List[int]) -> float:
         
         merged = []
         i = 0
@@ -31,9 +31,59 @@ class Solution:
             return merged[mlen//2]
         else:
             return (merged[mlen//2] + merged[mlen//2 - 1])/2
+    
+    def median(self, numList):
+        mid = len(numList) // 2 
+        if len(numList) % 2 == 0:
+            return (numList[mid - 1] + numList[mid]) / 2
+        else:
+            return numList[mid]
+
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+
+        if not nums1:
+            return self.median(nums2)
+        elif not nums2:
+            return self.median(nums1)
+        elif self.median(nums1) == self.median(nums2):
+            return self.median(nums1)
+        elif self.median(nums1) > self.median(nums2):
+            if len(nums2) == 1:
+                return self.median(nums1[:len(nums1)//2])
+            else:
+                return self.findMedianSortedArrays(nums1[:len(nums1)//2], nums2[len(nums2)//2:])
+        else:
+            if len(nums1) == 1:
+                return self.median(nums2[:len(nums2)//2])
+            else:
+                return self.findMedianSortedArrays(nums1[len(nums1)//2:], nums2[:len(nums2)//2])
 
 def test_simple():
     assert Solution().findMedianSortedArrays([2,6,8], [1,3,5,7]) == 5
 
 def test_empty_left():
     assert Solution().findMedianSortedArrays([], [3,4,5]) == 4
+
+def test_empty_right():
+    assert Solution().findMedianSortedArrays([3,6,10,11], []) == 8
+
+def test_asymmetric_high():
+    assert Solution().findMedianSortedArrays([100], [1, 2, 3, 4]) == 3
+
+def test_asymmetric_low():
+    assert Solution().findMedianSortedArrays([1, 2, 3], [0]) == 1.5
+
+def test_between():
+    assert Solution().findMedianSortedArrays([1,3,5,7], [9,12,15,18]) == 8
+
+def test_median_odd():
+    assert Solution().median([1,2,3]) == 2
+
+def test_median_even():
+    assert Solution().median([2,4,6,8]) == 5
+
+def test_base():
+    assert Solution().findMedianSortedArrays([], [1,2,3]) == 2
+
+if __name__ == "__main__":
+    test_simple()
